dbbac102ff7d677ecd0d43521015864705a578f9
sharifulislam565619 <sharifulislam565619@gmail.com>
2022-02-24 17:29:36 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useReducer, useRef, useState } from "react";
+import "./App.css";
+
+
+
+interface Todo {
+  id: number;
+  text: string;
+}
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
 
 function App() {
+  const [myState, setMyState] = useState<Todo>();
+
+
+  const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.text,
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+    }
+  }, []);
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value,
+      });
+      newTodoRef.current.value = "";
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" ref={newTodoRef} placeholder="todo text" />
+        </div>
+        <button onClick={onAddTodo}>Add</button>
+      </div>
+      <div className="todoList">
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <div className="task">
+              <div className="content">
+                <span>{todo.text}</span>
+              </div>
+              <button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>
+                Remove
+              </button>
+            </div>
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
